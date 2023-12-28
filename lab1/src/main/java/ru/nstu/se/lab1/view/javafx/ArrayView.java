e6fb4b6a02905fe3780aac05ee3af710179d6aea
kigo-qqw <alexandrvasyutin@gmail.com>
2023-12-28 13:32:43 +0700

    horizontal fix))))

--- lab1/src/main/java/ru/nstu/se/lab1/view/javafx/ArrayView.java
+++ lab1/src/main/java/ru/nstu/se/lab1/view/javafx/ArrayView.java
@@ -1,15 +1,10 @@
 package ru.nstu.se.lab1.view.javafx;
 
 import javafx.application.Platform;
-import javafx.beans.binding.DoubleBinding;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-
-import java.util.Objects;
-import java.util.Optional;
 
 public class ArrayView<T extends Comparable<T>> extends HBox {
     private ObservableList<T> data;
@@ -29,39 +24,16 @@ public class ArrayView<T extends Comparable<T>> extends HBox {
 
     void update() {
         getChildren().clear();
+        getChildren().addAll(this.data.stream()
+                .map(t -> {
+                    TextField textField = new TextField(t != null ? t.toString() : "");
+                    if (t == null) textField.setVisible(false);
+                    textField.setMaxWidth(40);
+                    textField.setEditable(false);
+                    return textField;
+                })
+                .toList());
 
-        Optional<T> maxElementOptional = this.data.stream().filter(Objects::nonNull).max(T::compareTo);
-        Optional<T> minElementOptional = this.data.stream().filter(Objects::nonNull).min(T::compareTo);
-
-        if (maxElementOptional.isPresent() && minElementOptional.isPresent()) {
-            T maxElement = maxElementOptional.get();
-            T minElement = minElementOptional.get();
-
-            double maxElementSize = sizingStrategy.getSize(maxElement);
-            double minElementSize = sizingStrategy.getSize(minElement);
-            double sizeRange = maxElementSize - minElementSize;
-
-            DoubleBinding minimalSize = widthProperty().divide(10);
-
-            getChildren().addAll(this.data.stream()
-                    .map(t -> {
-                        TextField textField = new TextField(t != null ? t.toString() : "");
-
-                        if (t == null) textField.setVisible(false);
-
-//                        textField.maxWidthProperty().bind(
-//                                widthProperty()
-//                                        .subtract(minimalSize)
-//                                        .divide(sizeRange)
-//                                        .multiply((t != null ? sizingStrategy.getSize(t) : 0) - minElementSize)
-//                                        .add(minimalSize)
-//                        );
-                        textField.setMaxWidth(40);
-                        textField.setEditable(false);
-                        return textField;
-                    })
-                    .toList());
-        }
     }
 
     public void highlightElementYellow(int index) {
